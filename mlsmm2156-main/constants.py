214fd4288b14b2752f6cd60df0adec105d2205a5
Lyrickst <cyril.demolin@gmail.com>
2025-04-17 15:33:02 +0200

    rajout de mes codes ;)

---
+++ mlsmm2156-main/constants.py
@@ -0,0 +1,29 @@
+# third parties imports
+from pathlib import Path
+
+
+class Constant:
+
+    DATA_PATH = Path('data/small')  # -- fill here the dataset size to use
+
+    # Content
+    CONTENT_PATH = DATA_PATH / 'content'
+    # - item
+    ITEMS_FILENAME = 'movies.csv'
+    ITEM_ID_COL = 'movieId'
+    LABEL_COL = 'title'
+    GENRES_COL = 'genres'
+
+    # Evidence
+    EVIDENCE_PATH = DATA_PATH / 'evidence'
+    # - ratings
+    RATINGS_FILENAME = 'ratings.csv'
+    USER_ID_COL = 'userId'
+    RATING_COL = 'rating'
+    TIMESTAMP_COL = 'timestamp'
+    USER_ITEM_RATINGS = [USER_ID_COL, ITEM_ID_COL, RATING_COL]
+    RATINGS_SCALE = (0.5, 5)  
+    
+
+    # Rating scale
+    RATINGS_SCALE = (0.5, 5)    # -- fill in here the ratings scale as a tuple (min_value, max_value)
